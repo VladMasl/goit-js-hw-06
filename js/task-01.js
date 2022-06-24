17d24dfe8455086e70b79be5ca88736cd1519400
Vlad  Masliuk <102538039+VladMasl@users.noreply.github.com>
2022-06-24 15:56:13 +0300

    +

--- js/task-01.js
+++ js/task-01.js
@@ -4,11 +4,17 @@ const titleEl = document.querySelectorAll("#categories .item h2");
 
 const elements = document.querySelectorAll("#categories .item ul");
 
+let listElLeng = [];
+
+const el = elements.forEach(list =>{
+    listElLeng.push(list.children.length) 
+})
+
 console.log("Number of categories:", listEl.length);
 
 console.log("Category:", titleEl[0].textContent);
-console.log("Elements:", elements[0].children.length);
+console.log("Elements:", listElLeng[0]);
 console.log("Category:", titleEl[1].textContent);
-console.log("Elements:", elements[1].children.length);
+console.log("Elements:", listElLeng[1]);
 console.log("Category:", titleEl[2].textContent);
-console.log("Elements:", elements[2].children.length);
+console.log("Elements:",listElLeng[2]);
